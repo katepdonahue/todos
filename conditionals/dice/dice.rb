976e348b5d8bae4ba82f9b3f0b99d9960f8eb037
Kate <katepdonahue@gmail.com>
2013-11-13 14:46:44 -0500

    add solution to dice

---
+++ conditionals/dice/dice.rb
@@ -0,0 +1,14 @@
+# Create a method in the dice class called `roll` that returns a random number
+# between 1 and 6
+
+#To test your solution run `rspec dice_spec.rb` in your terminal
+
+class Dice
+  def initialize
+  end
+
+  def roll
+    rand(1..6)
+  end
+
+end
