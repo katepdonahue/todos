5d67ae62a1c2cdc6e745fa25e564a65596fd0c99
Kate <katepdonahue@gmail.com>
2013-11-14 15:29:29 -0500

    Untrack decrytit and add .gitignore so file does not appear in global repo

--- random/encrypt/encryptit.rb
+++ random/encrypt/encryptit.rb
@@ -7,10 +7,19 @@ def encryptit(secret)
   sub_spaces = reverse_it.gsub(" ", "<")
   sub_e = sub_spaces.gsub("e", "^")
   # change the < to e's and the ^ to spaces
-  sub_back_e_where_spaces_were = sub_e.gsub("<", "e")
-  sub_back_spaces_where_e_was = sub_back_e_where_spaces_were.gsub("^", " ")
+  sub_back_e = sub_e.gsub("<", "e")
+  sub_back_spaces = sub_back_e.gsub("^", " ")
+  # take the first half (or slightly less than half if the secret has an odd number of chars)
+  # and move it to the end
+  first_half = sub_back_spaces[0..(secret.length/2)]
+  second_half = sub_back_spaces[((secret.length/2) + 1)..(secret.length)]
+  swap_beg_end = second_half + first_half
 end
 
 my_secret = "Hello world, this is Kate."
 
-puts encryptit(my_secret)
+mixed_secret = my_secret[((my_secret.length/2) + 1)..(my_secret.length)] + my_secret[0..(my_secret.length/2)]
+puts mixed_secret
+unmixed_secret = mixed_secret[((mixed_secret.length/2 + 1))..(mixed_secret.length)] + mixed_secret[0..(mixed_secret.length/2)]
+puts unmixed_secret
+# puts encryptit(my_secret)
