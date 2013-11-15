1f15b029e02bd7864670f84bd2c92107dfec8f18
Kate <katepdonahue@gmail.com>
2013-11-14 19:16:33 -0500

    refactor fizzbuzz code after checking out some other's online

--- Loops/fizzbuzz/fizzbuzz.rb
+++ Loops/fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,9 @@
-101.times do |i|
-  next if i == 0
-  if i % 3 == 0 && (i + 1) % 5 == 0
-    puts "fizzbuzz"
-  elsif i % 3 == 0
-    puts "fizz"
-  elsif i % 5 == 0
-    puts "buzz"
-  else
-    puts i
+(1..100).each do |i|
+  if i % 15 == 0   then puts "fizzbuzz"
+  elsif i % 3 == 0 then puts "fizz"
+  elsif i % 5 == 0 then puts "buzz"
+  else                  puts i
   end
 end
+
+# refactored based on: (1..100).each{|n|p n%15==0?"FizzBuzz":n%5==0?"Buzz":n%3==0?"Fizz":n}
