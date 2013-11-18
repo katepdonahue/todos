8490f28f897e8b6df6204c1d18bc38ab4b854bdd
Kate <katepdonahue@gmail.com>
2013-11-18 14:07:24 -0500

    Change .should syntax to expect syntax since it is more fashionable

--- Loops/fizzbuzz/fizzbuzz_spec.rb
+++ Loops/fizzbuzz/fizzbuzz_spec.rb
@@ -3,18 +3,18 @@ require "./fizzbuzz_method.rb"
 describe "#fizzbuzz" do
   
   it "should return 'fizzbuzz' when integer is divisible by 15" do
-    fizzbuzz(75).should eq("fizzbuzz")
+    expect(fizzbuzz(75)).to eq("fizzbuzz")
   end
 
   it "should return 'fizz' when integer is not divisible by 5 but is divisible by 3" do
-    fizzbuzz(27).should eq("fizz")
+    expect(fizzbuzz(27)).to eq("fizz")
   end
 
   it "should return 'buzz' when integer is not divisible by 3 but is by 5" do
-    fizzbuzz(25).should eq("buzz")
+    expect(fizzbuzz(25)).to eq("buzz")
   end
 
   it "should return the integer when it is not divisible by 3 or 5" do
-    fizzbuzz(13).should eq(13)
+    expect(fizzbuzz(13)).to eq(13)
   end
 end
