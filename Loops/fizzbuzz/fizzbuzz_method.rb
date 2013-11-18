d99a873e81a50791aafe32ab72b79f5bb81b9f0f
Kate <katepdonahue@gmail.com>
2013-11-18 14:34:36 -0500

    Monkey patch

--- Loops/fizzbuzz/fizzbuzz_method.rb
+++ Loops/fizzbuzz/fizzbuzz_method.rb
@@ -1,8 +1,11 @@
 # a fizzbuzz method:
-def fizzbuzz(integer)
-  if integer % 15 == 0   then "fizzbuzz"
-  elsif integer % 3 == 0 then "fizz"
-  elsif integer % 5 == 0 then "buzz"
-  else                        integer
+
+class Integer
+  def fizzbuzz
+    if self % 15 == 0   then "fizzbuzz"
+    elsif self % 3 == 0 then "fizz"
+    elsif self % 5 == 0 then "buzz"
+    else                     self
+    end
   end
 end
--- Loops/fizzbuzz/fizzbuzz_spec.rb
+++ Loops/fizzbuzz/fizzbuzz_spec.rb
@@ -1,26 +1,27 @@
 require "./fizzbuzz_method.rb"
 
-describe "#fizzbuzz" do
+describe Integer, "#fizzbuzz" do
   
   it "should return 'fizzbuzz' when integer is divisible by 15" do
-    expect(fizzbuzz(75)).to eq("fizzbuzz")
+    expect(75.fizzbuzz).to eq("fizzbuzz")
   end
 
   it "should return 'fizz' when integer is not divisible by 5 but is divisible by 3" do
-    expect(fizzbuzz(27)).to eq("fizz")
+    expect(27.fizzbuzz).to eq("fizz")
   end
 
   it "should return 'buzz' when integer is not divisible by 3 but is by 5" do
-    expect(fizzbuzz(25)).to eq("buzz")
+    expect(25.fizzbuzz).to eq("buzz")
   end
 
   it "should return the integer when it is not divisible by 3 or 5" do
-    expect(fizzbuzz(13)).to eq(13)
+    expect(13.fizzbuzz).to eq(13)
   end
 
   it "should raise an error if method called on a non-integer value" do
-    expect {fizzbuzz("poop")}.to raise_error
-    expect {fizzbuzz([2, 4])}.to raise_error
-    expect {fizzbuzz({:sym => value})}.to raise_error
+    expect {"poop".fizzbuzz}.to raise_error
+    expect {[2, 4].fizzbuzz}.to raise_error
+    expect {{:sym => value}.fizzbuzz}.to raise_error
   end
+
 end
