55a191cf5837884fdb0184a971900c2e39b3f85b
Kate <katepdonahue@gmail.com>
2013-11-18 13:19:18 -0500

    change puts to return

--- Loops/fizzbuzz/fizzbuzz_method.rb
+++ Loops/fizzbuzz/fizzbuzz_method.rb
@@ -1,8 +1,8 @@
 # a fizzbuzz method:
 def fizzbuzz(integer)
-  if integer % 15 == 0   then puts "fizzbuzz"
-  elsif integer % 3 == 0 then puts "fizz"
-  elsif integer % 5 == 0 then puts "buzz"
-  else                  puts integer
+  if integer % 15 == 0   then "fizzbuzz"
+  elsif integer % 3 == 0 then "fizz"
+  elsif integer % 5 == 0 then "buzz"
+  else                        integer
   end
 end
