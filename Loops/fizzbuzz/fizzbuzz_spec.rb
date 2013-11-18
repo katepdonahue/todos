114d121af0754415f3f073f36a7e9c9e62d682e7
Kate <katepdonahue@gmail.com>
2013-11-18 14:02:03 -0500

    change examples in spec to make tests a bit more rigorous looking

--- Loops/fizzbuzz/fizzbuzz_spec.rb
+++ Loops/fizzbuzz/fizzbuzz_spec.rb
@@ -3,18 +3,18 @@ require "./fizzbuzz_method.rb"
 describe "#fizzbuzz" do
   
   it "should return 'fizzbuzz' when integer is divisible by 15" do
-    fizzbuzz(15).should eq("fizzbuzz")
+    fizzbuzz(75).should eq("fizzbuzz")
   end
 
   it "should return 'fizz' when integer is not divisible by 5 but is divisible by 3" do
-    fizzbuzz(3).should eq("fizz")
+    fizzbuzz(27).should eq("fizz")
   end
 
   it "should return 'buzz' when integer is not divisible by 3 but is by 5" do
-    fizzbuzz(5).should eq("buzz")
+    fizzbuzz(25).should eq("buzz")
   end
 
   it "should return the integer when it is not divisible by 3 or 5" do
-    fizzbuzz(7).should eq(7)
+    fizzbuzz(13).should eq(13)
   end
 end
