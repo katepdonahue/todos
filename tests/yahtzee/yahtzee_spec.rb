f23617b7b72d768c196846b63ce8770bfa042912
Kate <katepdonahue@gmail.com>
2013-11-18 18:25:54 -0500

    Add carrot and $ to regex in spec to make sure only 5 digits not 5 digits in a string of more than 5 digits

--- tests/yahtzee/yahtzee_spec.rb
+++ tests/yahtzee/yahtzee_spec.rb
@@ -7,7 +7,7 @@ describe "#roll_dice" do
   it "should return an array with 5 elements, each a number between 1 and 6 (inclusive)" do
     # this test could pass if array has a nil elt and a two digit elt even though your code would be wrong
     # but with the compact in the test above, we made sure that the array has 5 non- nil elts
-    expect(roll_dice.join).to match_regex(/[1-6]{5}/)
+    expect(roll_dice.join).to match_regex(/^[1-6]{5}$/)
   end
 end
 
