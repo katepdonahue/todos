761cf1f3d15ef34dd11faf7389081e30d4c116fd
Kate <katepdonahue@gmail.com>
2013-11-20 09:21:32 -0500

    todo8: create a test for a method that turns elements of the array into a numbered list of strings

---
+++ todo8/list_maker_spec.rb
@@ -0,0 +1,7 @@
+require "./list_maker"
+
+describe Array, "#make_list" do
+  it "should return an array of strings in a numbered list" do
+    expect([2, 4, 7, 11].make_list).to eq(["1. 2", "2. 4", "3. 7", "4. 11" ])
+  end
+end
