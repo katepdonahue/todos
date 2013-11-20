f9cc370d365fdda70bc5d1b9f4096bf7cba7c73c
Kate <katepdonahue@gmail.com>
2013-11-20 13:31:08 -0500

    Write test for my_select method

--- other_assignments/yielding/my_collect_select_spec.rb
+++ other_assignments/yielding/my_collect_select_spec.rb
@@ -1,9 +1,16 @@
 require "./my_collect_select"
 
 describe Array do
+
   describe "#my_collect" do
     it "should return an array manipulated by the block" do
       expect([1, 2, 3].my_collect {|element| element + 1}).to eq([2, 3, 4])
+    end
+  end
+
+  describe "#my_select" do
+    it "should return an array with only the items that pass the condition in it" do
+      expect([1, 2, 3].my_select {|element| element > 1}).to eq([2, 3])
     end
   end
 end
