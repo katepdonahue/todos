6c549acaedf5206641740a1ddbac5026473cbc6c
Kate <katepdonahue@gmail.com>
2013-11-23 15:14:32 -0500

    Pass now_serving method test

--- todo89/deli_counter.rb
+++ todo89/deli_counter.rb
@@ -9,7 +9,14 @@ class Deli
     line << "#{line.length + 1}. #{name}"
   end
 
+  def now_serving
+    serving = line[0].split[1]
+    line.shift
+    serving
+  end
+
 end
 
-new_deli = Deli.new
-puts new_deli.line=(["Kate"])
+# new_deli = Deli.new
+# new_deli.line=(["1. Kate", "2. Nobody"])
+# # puts new_deli.now_serving
