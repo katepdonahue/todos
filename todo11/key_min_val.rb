f058d71527e3a8a9c43abf8e7ce5a9b396e68d6b
Kate <katepdonahue@gmail.com>
2013-11-26 09:06:30 -0500

    Write method to pass tests

---
+++ todo11/key_min_val.rb
@@ -0,0 +1,11 @@
+# prereqs: iterators, hashes, conditional logic
+# Given a hash with numeric values, return the key for the smallest value
+def key_for_min_value(hash)
+  smallest = hash[hash.keys.sample]
+  hash.each do |key, val|
+    if val < smallest
+      smallest = val
+    end
+  end
+  hash.key(smallest)
+end
