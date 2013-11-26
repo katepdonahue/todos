b2fe6649a35a7d697192c9b918a46ae32171c3db
Kate <katepdonahue@gmail.com>
2013-11-26 09:54:51 -0500

    Fix method to pass tests

--- todo11/key_min_val.rb
+++ todo11/key_min_val.rb
@@ -1,9 +1,16 @@
+require 'debugger'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(hash)
-  smallest = hash[hash.keys.sample]
-  hash.each do |key, val|
-      smallest = val if val < smallest
+  int_hash = hash.select { |key, val| val.class == Fixnum }
+  smallest = int_hash[int_hash.keys.sample]
+  # debugger
+  int_hash.each do |key, val|
+    if val < smallest
+      smallest = val 
+    end
   end
-  hash.key(smallest)
+  int_hash.key(smallest)
 end
+
+# p key_for_min_value({:nisha => 2, :kate => "orange", :gus => 2, :chris => 3})
--- todo11/spec.rb
+++ todo11/spec.rb
@@ -3,11 +3,11 @@ require "./key_min_val"
 describe "#key_for_min_value" do
 
   it "should return the key of the hash with the smallest value" do
-    expect(key_for_min_value({nisha: 2, kate: orange, gus: 4, chris: 3})).to eq(:nisha)
+    expect(key_for_min_value({nisha: 2, kate: "orange", gus: 4, chris: 3})).to eq(:nisha)
   end
 
-  it "should return the last smallest key if there is a tie in smallest values" do
-    expect(key_for_min_value({nisha: 2, kate: orange, gus: 2, chris: 3})).to eq(:gus)
+  it "should return the first smallest key if there is a tie in smallest values" do
+    expect(key_for_min_value({nisha: 2, kate: "orange", gus: 2, chris: 3})).to eq(:nisha)
   end
 
   it "should return nil if an empty hash is passed to it" do
