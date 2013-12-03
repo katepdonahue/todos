9ba2a2ace338960e75e81cc01809ca40616a0b70
Kate <katepdonahue@gmail.com>
2013-12-03 18:23:57 -0500

    Create to_int string method so that  don't become 0 with to_i

--- todo13/version_sort.rb
+++ todo13/version_sort.rb
@@ -1,15 +1,30 @@
+require 'debugger'
+
+class String
+
+  def to_int
+    self.to_i unless self == ""
+  end
+
+end
+
 def version_sort(file_array)
   to_sort = []
   new_array = []
   # use regex capture for first and last characters
   # capture numbers? Then can compare by accessing in match array?
   file_array.each do |file|
-    to_sort << (/foo-(\d*)\.{0,1}(\d*[a-z]{0,1})\.{0,1}(\d*)\.ext/).match(file)
+    to_sort << (/foo-(\d*)\.{0,1}(\d*)\.{0,1}(\d*)([a-z]{0,1})\.ext/).match(file)
   end
+
   sorted = to_sort.sort do |array1 ,array2| 
-    if (array1[1].to_i <=> array2[1].to_i) == 0
-      if (array1[2].to_i <=> array2[2].to_i) == 0
-        (array1[3].to_i <=> array2[3].to_i)
+    if (array1[1].to_int <=> array2[1].to_int) == 0
+      if (array1[2].to_int <=> array2[2].to_int) == 0
+        if (array1[3].to_int <=> array2[3].to_int) == 0
+          (array1[4] <=> array2[4])
+        else
+          (array1[3].to_i <=> array2[3].to_i)
+        end
       else
         (array1[2].to_i <=> array2[2].to_i)
       end
