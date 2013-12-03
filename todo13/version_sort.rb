bc6479edc0191f4bbe47bb23b5771d24399e1b51
Kate <katepdonahue@gmail.com>
2013-12-03 17:29:39 -0500

    subsort

--- todo13/version_sort.rb
+++ todo13/version_sort.rb
@@ -6,11 +6,18 @@ def version_sort(file_array)
   file_array.each do |file|
     to_sort << (/foo-(\d*)\.{0,1}(\d*[a-z]{0,1})\.{0,1}(\d*)\.ext/).match(file)
   end
-  # match array [whole thing, ||1st number, ||2nd number, ||letter, ||3rd number]
-  first = to_sort.sort_by { |array| array[1].to_i} # then split this into a bunch of arrays grouped based on same array[1] vals
-  #  second = to_sort.sort_by { |array| array[2].to_i } # how to move it to the front if it's nil
-  # third = second.sort_by { |array| array[3]}
-  first.each do |array|
+  sorted = to_sort.sort do |array1 ,array2| 
+    if (array1[1].to_i <=> array2[1].to_i) == 0
+      if (array1[2].to_i <=> array2[2].to_i) == 0
+        (array1[3].to_i <=> array2[3].to_i)
+      else
+        (array1[2].to_i <=> array2[2].to_i)
+      end
+    else
+      (array1[1].to_i <=> array2[1].to_i)
+    end
+  end
+  sorted.each do |array|
     new_array << array[0]
   end
   new_array
