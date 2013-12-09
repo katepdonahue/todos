f1a100c4a67235433b8b73960858605b6fa8c5c1
Kate <katepdonahue@gmail.com>
2013-12-09 09:18:38 -0500

    Write initial test

---
+++ todo16/count_array.rb
@@ -0,0 +1,6 @@
+class Array
+
+  def my_count
+  end
+
+end
--- todo16/spec.rb
+++ todo16/spec.rb
@@ -1,10 +1,22 @@
 # Instructions:
 # Count elements in an Array by returning a hash with keys of the elements and values of the amount of times they occur.
  
-# test = ['cat', 'dog', 'fish', 'fish']
+test = ['cat', 'dog', 'fish', 'fish']
  
 # def count(array)
 #   ___
 # end
  
 # count(test) #=> { 'cat' => 1, 'dog' => 1, 'fish' => 2 })
+
+require './count_array'
+
+describe Array do
+  describe "#my_count" do
+    it "should return a hash" do
+      expect(test.my_count.class).to eq(Hash)
+    end
+  end
+
+
+end
