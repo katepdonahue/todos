086853eab4a26dbb1d05660dafc702f990c9fdaf
Kate <katepdonahue@gmail.com>
2013-12-09 09:26:14 -0500

    Make test pass

--- todo16/count_array.rb
+++ todo16/count_array.rb
@@ -3,7 +3,11 @@ class Array
   def my_count
     hash = Hash.new
     self.each do |elt|
-      hash[elt] = 1
+      if hash[elt]
+        hash[elt] += 1
+      else
+        hash[elt] = 1
+      end
     end
     hash
   end
