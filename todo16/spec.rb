c9cdcdd5b6d0eb4b1137f7174b169bb1caba7896
Kate <katepdonahue@gmail.com>
2013-12-09 09:21:26 -0500

    Make test more precise

--- todo16/spec.rb
+++ todo16/spec.rb
@@ -7,14 +7,14 @@ test = ['cat', 'dog', 'fish', 'fish']
 #   ___
 # end
  
-# count(test) #=> { 'cat' => 1, 'dog' => 1, 'fish' => 2 })
+test_hash = { 'cat' => 1, 'dog' => 1, 'fish' => 2 }
 
 require './count_array'
 
 describe Array do
   describe "#my_count" do
-    it "should return a hash" do
-      expect(test.my_count.class).to eq(Hash)
+    it "should return a hash with keys of the elements and values of the amount of times they occur" do
+      expect(test.my_count).to eq(test_hash)
     end
   end
 
