bbd50b559d9878a4b2bf25a23d5fc9faa9b8e982
Kate <katepdonahue@gmail.com>
2013-12-13 09:42:58 -0500

    Think more

--- todo18/fib.rb
+++ todo18/fib.rb
@@ -2,5 +2,6 @@
 # create a method that returns the nth number of the fibonacci sequence
 # fib sequence = 0,1,1,2,3,5,8,13 etc, assume the 1st element is 0 (the first number), the 2nd element is 1, the third element is 1
 def fibo_finder(n)
-  # code goes here
+  fib_array = [0, 1]
+  fib_array[2] = fib_array[1] + fib_array[0]
 end
