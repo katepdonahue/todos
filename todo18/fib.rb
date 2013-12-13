acc21ab5b52e2e69728acf0f5852ddbf218401ea
Kate <katepdonahue@gmail.com>
2013-12-13 09:48:50 -0500

    Create fib_array with enough entries and pick the last one

--- todo18/fib.rb
+++ todo18/fib.rb
@@ -3,5 +3,8 @@
 # fib sequence = 0,1,1,2,3,5,8,13 etc, assume the 1st element is 0 (the first number), the 2nd element is 1, the third element is 1
 def fibo_finder(n)
   fib_array = [0, 1]
-  fib_array[2] = fib_array[1] + fib_array[0]
+  (2..n).each do |i|
+    fib_array << fib_array[i-1] + fib_array[i-2]
+  end
+  fib_array[n]
 end
