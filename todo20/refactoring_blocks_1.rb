f0750d3c055da2c47278c15e896802ebb5198895
Kate <katepdonahue@gmail.com>
2013-12-16 10:23:55 -0500

    Thanks to Sarah for showing me how to do this

--- todo20/refactoring_blocks_1.rb
+++ todo20/refactoring_blocks_1.rb
@@ -26,22 +26,25 @@ def pay_by(order)
   order.compute_cost
   order.compute_shipping
   order.compute_tax
-  yield
+  yield(order)
   order.ship_goods
 end 
 
 def pay_by_visa(order,ccn)
-  pay_by(order) {|order| order.verify_payment}
+  pay_by(order) {|order| order.payment :type => :visa , :ccn => ccn; order.verify_payment}
 end
  
 def pay_by_check(order)
-  pay_by(order) {|order| order}
+  pay_by(order) {|order| order.payment :type => :check , :signed => true}
 end
  
 def pay_by_cash(order)
-  pay_by(order) {|order| order}
+  pay_by(order) {|order| order.payment :type => :cash}
 end
  
-def pay_by_store_credit(order)
-  pay_by(order) {|order| current_user.store_credit -= order.cost}   # current_user is a method with no params (ie, the customer)
+def pay_by_store_credit(order, current_user)
+  pay_by(order) do |order| 
+    order.payment :type => :store_credit
+   current_user.store_credit -= order.cost
+ end
 end
