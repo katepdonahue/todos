d511e9826d7c88065776bd089f89c06f41b8173c
Kate <katepdonahue@gmail.com>
2013-12-17 12:31:38 -0500

    reassign element so that it doesn't get deleted which would mess up the each iteration

--- todo21/unique.rb
+++ todo21/unique.rb
@@ -4,12 +4,14 @@ class Array
  
   def uniq
     uniq_array = []
-    self.each do |elt|
+    self.each_with_index do |elt, i|
       uniq_array << elt
-      if array.count(elt) > 1
+      if self.count(elt) > 1
+        self[i] = "place holder"
         delete(elt)
       end
     end
+    uniq_array
   end
  
 end
