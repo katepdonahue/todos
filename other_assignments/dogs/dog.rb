961e176fc346764461b3adeff8ff855456e701a5
Kate <katepdonahue@gmail.com>
2013-12-17 13:39:42 -0500

    Create dogs dotabase with dogs table using sql pro and add initialize method

---
+++ other_assignments/dogs/dog.rb
@@ -0,0 +1,40 @@
+# CREATE TABLE dogs
+# (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+# name TEXT,
+# color TEXT);
+
+require 'mysql2'
+ 
+class Dog
+  attr_accessor :name, :color
+  @@db = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "dogs")
+
+  def initialize(name, color)
+    @name = name
+    @color = color
+  end
+ 
+end
+ 
+dog = Dog.find(10)
+debugger
+puts 'hi'
+ 
+  # color, name, id
+  # db
+  # find_by_att
+  # find
+  # insert
+  # update
+  # delete/destroy
+ 
+  # refactorings?
+  # new_from_db?
+  # saved?
+  # save! (a smart method that knows the right thing to do)
+  # unsaved?
+  # mark_saved!
+  # ==
+  # inspect
+  # reload
+  # attributes
