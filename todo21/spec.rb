24a9726637b82c35dd117d34786c7b11335078c1
Kate <katepdonahue@gmail.com>
2013-12-17 12:20:48 -0500

    Make sure tests pass on ruby unique method

---
+++ todo21/spec.rb
@@ -0,0 +1,15 @@
+# require './unique'
+
+describe Array, "#uniq" do
+
+  it "should work on integers (return the array with all duplicate values dropped)" do
+    array = [1, 1, 2, 3, 7, 5, 7]
+    expect(array.uniq).to eq([1, 2, 3, 7, 5])
+  end
+
+  it "should work on strings (return the array with all duplicate values dropped)" do
+    array = ["hello", "hello", "my", "name", "is", "name"]
+    expect(array.uniq).to eq(["hello", "my", "name", "is"])
+  end
+
+end
