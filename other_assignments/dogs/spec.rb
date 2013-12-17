c86f100d3f2c34c5327d33aa2fa5165a5e8dd292
Kate <katepdonahue@gmail.com>
2013-12-17 14:28:07 -0500

    Write test for find method

--- other_assignments/dogs/spec.rb
+++ other_assignments/dogs/spec.rb
@@ -12,12 +12,20 @@ describe Dog do
     end
   end
 
-  describe "#update" do
-
+  describe "#find" do
+    it "should find a dog in the database and return it as a dog object" do
+      row_hash = {"id" => 1, "name" => "Spot", "color" => "Gray"}
+      expect(Dog.find(1)).to eq(row_hash)
+    end
   end
 
-  describe "#find" do
-
+  describe "#update" do
+    # it "should insert a dog object into the database" do
+    #   dog1.update
+    #   sql_command = "SELECT * FROM dogs WHERE dogs.name = '#{dog1.name}';"
+    #   row_hash = {"id" => 1, "name" => "Spot", "color" => "Gray"}
+    #   expect(Dog.db.query(sql_command).first).to eq(row_hash)
+    # end
   end
 
   describe "#find_by_name" do
