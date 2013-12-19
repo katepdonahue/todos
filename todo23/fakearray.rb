46588f77a47af738573f0d111c74abcd9f9eb2bf
Kate <katepdonahue@gmail.com>
2013-12-19 09:31:05 -0500

    Add a yield to my each method

--- todo23/fakearray.rb
+++ todo23/fakearray.rb
@@ -1,6 +1,7 @@
  
 class FakeArray
-
+  attr_accessor :elt1, :elt2, :elt3
+  
   def initialize(elt1, elt2, elt3)
     @elt1 = elt1
     @elt2 = elt2
@@ -8,7 +9,7 @@ class FakeArray
   end
  
   def each
-    
+    yield(self.elt1, self.elt2, self.elt3)
   end
  
   def first
