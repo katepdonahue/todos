e0852d4ce37e7b29569b56b7797d553b0af1578e
Kate <katepdonahue@gmail.com>
2014-01-05 22:56:08 -0500

    Initial Commit

---
+++ todo29/spec.rb
@@ -0,0 +1,6 @@
+require './each_with_object'
+
+describe "even_sum" do
+  it "should return return an array containing one reversed string for each string that has an even number of characters." do
+  end
+end
