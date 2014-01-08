30e5abd9735ac5dbccee7e2d8f074654852e4bfd
Kate <katepdonahue@gmail.com>
2014-01-08 09:00:01 -0500

    Fix spec format

--- todo31/spec.rb
+++ todo31/spec.rb
@@ -1,10 +1,14 @@
 # A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
 # longest_palindrome("afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd") #=> "dhfdkjfffhhfffjkdfhd"
 
-require '/palindrome'
+require './palindrome'
 
 describe String do
-  describe "longest_palindrome" do
 
+  describe "#longest_palindrome" do
+    it "should return the longest palindrome in a given string" do
+      string1 = "afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd"
+      expect(string1.longest_palindrome).to eq("dhfdkjfffhhfffjkdfhd")
+    end
   end
 end
