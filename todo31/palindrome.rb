54674279d4e441dddc60c76f017ac97e3b9caa16
Kate <katepdonahue@gmail.com>
2014-01-08 11:32:21 -0500

    It works

--- todo31/palindrome.rb
+++ todo31/palindrome.rb
@@ -4,14 +4,13 @@ class String
 
   def longest_palindrome
     palindromes = []
-    ((self.size)-1).times do |first|
-      ((first+1)..((self.size)-1)).each do |last|
+    (self.size-1).times do |first|
+      (first+1..self.size-1).each do |last|
         section = self[first..last]
         palindromes << section.reverse if section == section.reverse
       end
     end
-    answers = palindromes.sort_by { |palindrome| palindrome.length }
-    answers.last
+    palindromes.sort_by { |palindrome| palindrome.length }.last
   end
 
 end
