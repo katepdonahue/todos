f7f76b7522c1f2191a1dc31a571edba9a0ee8f89
Kate <katepdonahue@gmail.com>
2014-01-08 10:08:43 -0500

    Range before .each has to be in parenthesis to not return errors

--- todo31/palindrome.rb
+++ todo31/palindrome.rb
@@ -1,16 +1,17 @@
- 
+require "debugger"
+
 class String
 
   def longest_palindrome
     palindromes = []
-    (self.size).times do |i|
-      [(i+1)..(self.size-1)].each do |j|
-        section = self[i..j]
+    ((self.size)-1).times do |first|
+      ((first+1)..((self.size)-1)).each do |last|
+        section = self[first..last]
         palindromes << section.reverse if section == section.reverse
       end
     end
-    # palindromes.sort_by size
-    palindromes.last
+    answers = palindromes.sort_by { |palindrome| palindrome.length }
+    answers.last
   end
 
 end
