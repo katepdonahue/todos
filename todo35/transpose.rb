1b1ef9f80bc43e1b6b54840641857ef5fe5a25b2
Kate <katepdonahue@gmail.com>
2014-01-15 09:22:09 -0500

    A bit hacky

--- todo35/transpose.rb
+++ todo35/transpose.rb
@@ -2,12 +2,14 @@ class Array
 
   def my_transpose
     outer_array = []
-    self[0].size.times do |i|
-      inner_array = []
-      self.each do |array|
-        inner_array << array[i]
+    if self[0]
+      self[0].size.times do |i|
+        inner_array = []
+        self.each do |array|
+          inner_array << array[i]
+        end
+        outer_array << inner_array
       end
-      outer_array << inner_array
     end
     outer_array
   end
