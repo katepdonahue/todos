3458d615c107e316d480d960c26edf9d451fd655
Kate <katepdonahue@gmail.com>
2014-01-15 09:03:18 -0500

    Correct method names

--- todo35/spec.rb
+++ todo35/spec.rb
@@ -6,9 +6,9 @@ describe Array "#my_transpose" do
     expect([[1, 2, 3], [:a, :b, :c]].my_transpose).to eq([[1, :a], [2, :b], [3, :c]])
   end
   it "should create new arrays with each set of corresponding indices given three arrays" do
-    expect([[1,2], [3,4], [5,6]].transpose).to eq([[1, 3, 5], [2, 4, 6]])
+    expect([[1,2], [3,4], [5,6]].my_transpose).to eq([[1, 3, 5], [2, 4, 6]])
   end
   it "should create new arrays with each set of corresponding indices given an empty array" do
-    expect([].transpose).to eq([])
+    expect([].my_transpose).to eq([])
   end
 end
