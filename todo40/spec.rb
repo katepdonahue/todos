b11d1dbfe52509fb0805659de3fe5ee3962fdee7
Kate <katepdonahue@gmail.com>
2014-01-20 09:17:35 -0500

    Write spec

--- todo40/spec.rb
+++ todo40/spec.rb
@@ -1,9 +1,19 @@
-Separate numbers with a comma each three digits.
- 
-def separate_with_comma(n)
-  #code goes here
+# Separate numbers with a comma each three digits.
+
+require './commas'
+
+describe "#separate_with_comma" do
+
+  it "should change an integer to a string" do
+    expect(separate_with_comma(1)).to eq("1") 
+  end
+
+  it "should add a comma in the right spot 1,000" do
+    expect(separate_with_comma(1000)).to eq("1,000")
+  end
+
+  it "should add a coma in the right spot 10,000" do
+    expect(separate_with_comma(10000)).to eq("10,000")
+  end
+
 end
- 
-separate_with_comma(1) #=> "1" 
-separate_with_comma(1000) #=> "1,000" 
-separate_with_comma(10000) #=> "10,000"
